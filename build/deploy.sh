d8ff415db1e6794c5cf0acfe5c88cb2f48e078fe
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2021-12-03 17:18:20 +0100

    Change report mail address

--- build/deploy.sh
+++ build/deploy.sh
@@ -5,4 +5,4 @@ export LC_ALL=C.UTF-8
 echo -e "Content-type: text/html\n"
 
 OUTPUT+=$(cd .. && ./kleobis-deploy.sh 2>&1)$'\n\n'
-echo "$OUTPUT" | mail "me@apophis.cz" -s "[ksi-monitoring] Deploy status"
+echo "$OUTPUT" | mail "ksi-admin@fi.muni.cz" -s "[ksi-monitoring] Deploy status"
